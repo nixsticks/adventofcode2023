7bcb1d74b5ade3d49f99e1ddaf1ae5d58908f483
Nikki Thean <nikki.thean@gmail.com>
2023-12-06 17:30:45 -0800

    Add comments

--- 12-5/solution.py
+++ 12-5/solution.py
@@ -29,22 +29,30 @@ def get_range_overlap(seed_ranges, resources):
                 for resource_range in resource:
                     d1, s1, span = resource_range
                     s2 = s1 + span
-                    offset = d1 - s1
+                    # If the current range does not overlap with the source range
                     if s2 <= r1 or r2 <= s1:
                         continue
+                    # If the current range starts before the source range, put the subrange on the stack
+                    # for later and shorten the current range under examination
                     if r1 < s1:
                         current_ranges.append((r1, s1))
                         r1 = s1
+                    # If the current range extends beyond the source range, put the subrange on the stack
+                    # for later and shorten the current range under examination
                     if s2 < r2:
                         current_ranges.append((s2, r2))
                         r2 = s2
-                    destination_ranges.append((r1 + offset, r2 + offset))
+                    # Append our current result to the destination ranges to be examined when we move to the 
+                    # next map, adding the offset required to change it to the destination range
+                    destination_ranges.append((r1 + (d1 - s1), r2 + (d1 - s1)))
                     break
                 else:
+                    # The current range did not overlap with any of the source ranges so append as is
                     destination_ranges.append((r1, r2))
             current_ranges = destination_ranges
             destination_ranges = []
-        location_ranges += current_ranges # what is left over at the end
+        # Whatever is left over at the end is the location ranges
+        location_ranges += current_ranges
     return location_ranges
 
 
